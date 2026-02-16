31ca138deb1e349badcbb80348816ca6d6b29362
Nikita-hub6377 <nikitakumaripanda2102@gmail.com>
2026-02-16 12:59:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+# config.py
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",
-    "password": "Nikita@123",  # Based on your mysql server password or if incase xampp is used by someone then ("password": "" )keep it as null 
-    "database": "BLOOD_DONOR_FINDER" # Match your SQL script name
+    "password": "Nikita@123",  
+    "database": "blood_donor_finder" # Match your SQL script name
 }
 
 SECRET_KEY = "supersecretkey"
