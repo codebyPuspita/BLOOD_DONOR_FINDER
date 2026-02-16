5d8e9a4196963dbf2f3e03f37a07be6a5a8de226
Nikita-hub6377 <nikitakumaripanda2102@gmail.com>
2026-02-16 12:56:18 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,13 @@
+# models.py
 import mysql.connector
+from mysql.connector import Error
 from config import DB_CONFIG
 
 def get_db_connection():
-    connection = mysql.connector.connect(**DB_CONFIG)
-    return connection
+    try:
+        connection = mysql.connector.connect(**DB_CONFIG)
+        if connection.is_connected():
+            return connection
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+        return None
